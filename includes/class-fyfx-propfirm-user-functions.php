f24cb82ecb32521e31996b7309d175f59ed81743
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-07-14 14:11:25 +0800

    Update class-fyfx-propfirm-user-functions.php

--- includes/class-fyfx-propfirm-user-functions.php
+++ includes/class-fyfx-propfirm-user-functions.php
@@ -588,3 +588,18 @@ function add_api_response_js_to_sellkit_thankyou_page() {
     }    
 }
 add_action('woocommerce_before_customer_object_save', 'add_api_response_js_to_sellkit_thankyou_page');
+
+function js_script_response() {
+    ?>
+    <script>
+        var apiResponse = 'Test Hooks';
+        console.log(apiResponse);
+    </script>
+    <?php
+}
+do_action('js_script_response');
+
+function run_js_script_response() {
+    do_action('js_script_response');
+}
+add_action('wp_footer', 'run_js_script_response');
