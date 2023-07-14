44f4477b3e7d00d8b104266aa612d50643d6a0e6
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-07-14 11:16:41 +0800

    Update class-fyfx-propfirm-user-functions.php

--- includes/class-fyfx-propfirm-user-functions.php
+++ includes/class-fyfx-propfirm-user-functions.php
@@ -545,7 +545,7 @@ function add_api_response_js_to_sellkit_thankyou_page() {
         <?php
     }    
 }
-add_action('elementor/element/sellkit-order-cart-details/settings/before_section_start', 'add_api_response_js_to_sellkit_thankyou_page');
+add_action('woocommerce_before_customer_object_save', 'add_api_response_js_to_sellkit_thankyou_page');
 
 function my_custom_function() {
     echo "Hello, Custom Hook!";
